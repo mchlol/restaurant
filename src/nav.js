f03dcf0a6d6472c46129337f1196ce384af00e45
mchlol <m.andreabr@gmail.com>
2022-07-31 13:43:37 +1000

    begin converting nav li items into buttons and set up functionality

--- src/nav.js
+++ src/nav.js
@@ -1,5 +1,20 @@
 function nav() {
     const nav = document.createElement('nav');
+
+    const navList = document.createElement('ul');
+
+    // CONVERT LI TO BUTTON
+    // on click, these buttons should remove the currently displayed content and load the relevant content
+    const homeLink = document.createElement('li');
+    homeLink.textContent = 'Home';
+    const menuLink = document.createElement('li');
+    menuLink.textContent = 'Menu';
+    const contactLink = document.createElement('li');
+    contactLink.textContent = 'Contact';
+
+
+
+
     nav.innerHTML = `
     <ul>
                 <a href="./home.html">
@@ -13,6 +28,8 @@ function nav() {
                 </a>
             </ul>
     `;
+
+    nav.appendChild(navList);
     return nav;
 };
 
